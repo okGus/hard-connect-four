d252199911866418d5d3701be2bd5545c50d2587
Gustavo <rosalesagustavo@gmail.com>
2021-05-10 19:44:22 -0400

    updated incoming fixes

--- RedCoin.java
+++ RedCoin.java
@@ -1,11 +1,8 @@
 public class RedCoin extends Coin {
-<<<<<<< HEAD
-=======
 
     /**
      * Initializes a newly created RedCoin object
      */
->>>>>>> main
     public RedCoin() {
         super("Red");
     }
--- YellowCoin.java
+++ YellowCoin.java
@@ -1,11 +1,8 @@
 public class YellowCoin extends Coin {
-<<<<<<< HEAD
-=======
 
     /**
      * Initializes a newly created YellowCoin object
      */
->>>>>>> main
     public YellowCoin() {
         super("Yellow");
     }
